eaee1a97af6a81d0a39789d3030516c0fa2250d4
Naveen <92085148+unakal@users.noreply.github.com>
2021-10-14 11:21:55 +0530

    Delete Donar directory

--- Donar/src/main/java/com/example/demo/repository/DonarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Donar;
-
-public interface DonarRepository extends JpaRepository<Donar, Integer>{
-
-}
